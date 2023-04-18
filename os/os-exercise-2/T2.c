180c8af41893d4b0ce2de54e9732a69940b9f37e
Sebastian Lenzlinger <baschi99@windowslive.com>
2023-04-18 14:04:29 +0200

    Add mean and median for T2.c

--- os/os-exercise-2/T2.c
+++ os/os-exercise-2/T2.c
@@ -6,6 +6,10 @@
 
 void swap(int *xp, int *yp);
 void sort(int arr[], int size);
+float mean(int *arr, int size);
+float median(int *arr, int size);
+
+
 
 struct thread_data {
     int * arr;
@@ -39,9 +43,6 @@ int main (){
 	// One thread should display the result (mean, median)
 }
 
-void * mean (void * args) {
-}
-
 void swap(int *xp, int *yp)
 {
     int temp = *xp;
@@ -59,3 +60,30 @@ void sort(int *arr, int size)
            if (arr[j] > arr[j+1])
               swap(&arr[j], &arr[j+1]);
 }
+
+void * mean(int *arr, int size)
+{
+    int i;
+    float res;
+    float sum = 0;
+    for (i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    res = sum / size;
+    return res;
+}
+
+//Return float for even length array since median then mean of two middle values. Assumes Array size>0.
+void * median(int *arr, int size)
+{
+    float res; 
+    sort(arr,size);
+    if (size % 2 != 0) {
+        res = arr[size/2];
+    } else {
+        res = (arr[size/2-1] + arr[size/2])/2.0;// 2.0 to force floating point division
+    }
+    return res;
+}
+
+
