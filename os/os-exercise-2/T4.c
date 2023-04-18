965dd60ab7581516f81f9aaa68e6c27b58c6164f
Sebastian Lenzlinger <baschi99@windowslive.com>
2023-04-18 18:20:14 +0200

    Add comments to T4.c

--- os/os-exercise-2/T4.c
+++ os/os-exercise-2/T4.c
@@ -17,14 +17,15 @@ void schedule_FCFS(struct process *);
 void schedule_SJF(struct process *);
 void schedule_LJF(struct process *);
 
+//Compare based on arrival time for FCFS. 
 int cmpr (const void * a, const void * b){
     return ((struct process*)a)->arrival_time - ((struct process *)b)->arrival_time;
 }
-
+//Compare based on execution time for SJF. 
 int cmpr_SJF(const void * a, const void *b){
     return ((struct process*)a)->execution_time - ((struct process*)b)->execution_time;
 }
-
+//like cmpr_SJF but inverse, since now we want the longer exec time to be first
 int cmpr_LJF(const void * a, const void *b){
     return  -1*(((struct process*)a)->execution_time - ((struct process*)b)->execution_time);
 }
@@ -77,18 +78,29 @@ void display(struct process * input){
 
 void schedule_FCFS(struct process * arr)
 {
+        //Sort array according to policy
 	qsort(arr, SIZE, sizeof(struct process), cmpr);
         int i;
         int diff;
         int arrival;
-        arr[0].start_time = 0;
+        // process arr[0] starts when it arrives. Allways at time 0 in this case.
+        arr[0].start_time = arr[0].arrival_time;
+        // loop over arr calculating the respective start times
         for (i = 1; i < SIZE; i++) {
+            /*We assume a process cannot start the same time an other end, hence +1. 
+             * diff is the earliest possible start time. 
+             * If process arrives after it will start when it arrives. 
+             * (Note: arrival and start are atomic in this implementation i.e. can happen at the same time. */
            diff = arr[i-1].start_time + arr[i-1].execution_time + 1;
            arrival = arr[i].arrival_time;
+           //cannot start before arrival
            arr[i].start_time = diff>arrival?diff:arrival;
         }
 	printf("Implemented!\n");
 }
+
+/* each scheduler just uses theri respective cmpr function which essentially dictates the scheduling. 
+ * Otherwise the considerations are like for FCFS, so we don't comment them to avoid redundancy */
 void schedule_SJF(struct process * arr)
 {
 
@@ -127,6 +139,7 @@ void display_average_waiting_time(struct process * arr)
         int total = 0;
         int i;
         for (i = 0; i < SIZE; i++) {
+            //Start time must be >= arrival time. so we dont take abs value
                total += arr[i].start_time - arr[i].arrival_time;
               // printf("Current total: %d. Start: %d. Arrival %d\n", total, arr[i].start_time, arr[i].arrival_time);
         }
